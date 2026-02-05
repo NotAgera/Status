bd3e13c5a52efb8b6d109bcc06f23aa40bf64705
NotAgera <emilsh99@hotmail.com>
2026-02-05 10:33:48 +0100

    Add core.py with double_value function

---
+++ logic/core.py
@@ -0,0 +1,9 @@
+# logic/core.py
+
+def double_value(value: float) -> float:
+    """Eksempel: dobler tallet. Bytt dette med ekte logikk."""
+    return value * 2
+
+# Legg gjerne inn flere funksjoner her:
+# def generate_report(data: dict) -> dict: ...
+# def compute_something(a: int, b: int) -> float: ...
